73cc043edf5b036f4acab557cff911dd60287639
Jonathan Perkins <jonathanperkins30@gmail.com>
2017-03-04 10:32:49 +0000

    Update Leonardo.ino

--- MAIN/Leonardo.ino
+++ MAIN/Leonardo.ino
@@ -1,12 +1,39 @@
+// ******************** LINE FOLLOWER ROBOT *********************
+// The robot must complete the following tasks:
+//
+// - Follow a black line.
+// - Jump a gap in the black line.
+// - If there is a yellow line junction on the black line, the
+//   robot must go down it, light up an LED to show the colour
+//   at the end of the yellow line. Then turn around and continue
+//   the journey on the black line.
+
 #include <QTRSensors.h>
 #include <Wire.h>
 #include <Servo.h>
 
-Servo servol;
-Servo servor;// create servo object to control a servo
+// SERVO MOTOR SPEEDS
+//
+// 94 ==  LEFT Stopped Counterclockwise.
+// 93 ==  RIGHT Stopped Clockwise.
 
+// 104 ==  LEFT Full Speed Clockwise.
+// 83 ==  RIGHT Full Speed Counterclockwise.
+
+// IR REFLECTANCE SENSOR OUTPUT
+//
+// 1000 == Sensor[n] is over the black line.
+// >0, <1000 == Sensor[n] is over the white surface.
+// 0 == Sensor[n] doesn't recognise anything. (Calibration issue). 
+
+// Create servo objects.
+Servo servol;
+Servo servor;
+
+// Define the pins used by the IR reflectance sensor.
 QTRSensorsRC qtr((char[]) {2, 3, 4, 5, 6, 7, 8, 9}, 8); // {Digital Pin Numbers}
 
+// Analogue pins used as digital to read incoming colours from the Nano.
 #define BLUE A0
 #define RED A1
 #define BLACK A2
@@ -15,19 +42,28 @@ QTRSensorsRC qtr((char[]) {2, 3, 4, 5, 6, 7, 8, 9}, 8); // {Digital Pin Numbers}
 void setup() {
   Serial.begin(9600);
 
+  // IR LEDs pin.
   pinMode(10, OUTPUT);
+  // Builtin LED
   pinMode(13, OUTPUT);
-  
+
+  // Setup incoming colour pins.
   pinMode(BLUE, INPUT);
   pinMode(RED, INPUT);
   pinMode(YELLOW, INPUT);
   pinMode(BLACK, INPUT);
-  
+
+  // Turn on IR LEDs.
   digitalWrite(10, HIGH);
 
   // IR Sensor Calibration
+  //
+  // Move the sensor left and right over the line so that
+  // all IR sensors know the difference between the surface
+  // and the white line. This only needs to done once.
+  //
   int i;
-  for (i = 0; i < 250; i++) // make the calibration take about 5 seconds
+  for (i = 0; i < 250; i++)
   {
   qtr.calibrate();
   qtr.emittersOn();
@@ -37,56 +73,73 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
   //digitalWrite(A5, LOW);
   }
-  
+
+  // Attaches the servo objects to pins.
   servor.attach(11);
-  servol.attach(12);// attaches the servo on pin 9 to the servo object
+  servol.attach(12);
 
 
 }
 
 void loop() {
 
-  digitalWrite(LED_BUILTIN, LOW);
-  //digitalWrite(A5, LOW);
+  // Define turn variable.
+  // Left = 0;
+  // Right = 1;
   int turns;
-  int byteRead;
-  int incomingByte = 0;
+  
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Read from the IR sensor. Then determine the error (how far away from the line)
+  // the robot is.
   digitalWrite(10, HIGH);   
   unsigned int sensors[8];
   int position = qtr.readLine(sensors);
   int error = position - 1000;
-  
+
+  // Print sensor and/or Colour values to the serial for debugging.
   //SensorPrint();
   ColourPrint();
   
-  
+
+  // Blink the builtin LED. Shows something is running!
   if (sensors[0] >= 0)
   {digitalWrite(LED_BUILTIN, HIGH); delay(50); digitalWrite(LED_BUILTIN, LOW);}
 
-  servor.write(83);       //goes straight if it doesnt match anything
+
+  // Let the robot travel straight intialally or is perfectly over the line (unlikely). 
+  servor.write(83);
   servol.write(104);
 
-// 94 =  LEFT Stopped Counterclockwise.
-// 93 =  RIGHT Stopped Clockwise.
-
-// 104 =  LEFT Full Speed Clockwise.
-// 83 =  RIGHT Full Speed Counterclockwise.
-
+  // Turn the robot LEFT if the Error is greater than 3000.
   if (error > 3000){
        servol.write(94); // Stopped L, Turning Left
        turns = 0;
   }
+  
+  // Turn the robot RIGHT if the Error is greater than 3000.
   if (error<3000){
        servor.write(93); // Stopped R, Turning Right
        turns = 1;
   }
 
+  // Check if the incoming colour from the Nano is Yellow.
+  // If this is true the Colour(turn_state, jumped_gap) function
+  // is run.
   if (digitalRead(YELLOW) == HIGH)
   {
     Serial.println("Reading Yellow");
     Colour(turns, 0);
   }
 
+
+// INITIAL GAP CHECKING
+//
+// If the robot stops seeing the line, it will travel forward for a small amount of time. If the line is still not to be found after this time,
+// the Gap() function is run. The delay stops the robot from instantly going into the gap function due to bad sections on the black line.
+//
+// After the gap function has finished and has found the black line (returns true), the robot continues it's journey using the Drive() function.
+//
 if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4] != 1000 && sensors[5] != 1000 && sensors[6] != 1000 && sensors[7] != 1000)
 {
   for (unsigned int i = 0; i < 20; i++)
@@ -116,12 +169,14 @@ if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4]
 }
 }
 
+// An endless loop of following the black line and checking for a yellow line. This function
+// is only run after the gap function has returned true.
+//
 void Drive()
 {
-  //digitalWrite(A0, LOW);
   while(1)
   {
-    ColourPrint();
+    //ColourPrint();
     int turns;
     unsigned int sensors[8];
     int position = qtr.readLine(sensors);
@@ -150,12 +205,14 @@ void Drive()
 
 bool Gap()
 {
+  // Read the IR values again.
   unsigned int sensors[8];
   int position = qtr.readLine(sensors);
   int error = position - 1000;
   digitalWrite(10, HIGH);
-  //digitalWrite(A0, HIGH);
-  
+
+  // Check if over the line and turn LEFT.
+  //
   if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4] != 1000 && sensors[5] != 1000 && sensors[6] != 1000 && sensors[7] != 1000)
   {
     servol.write(94); // Stopped L
@@ -163,6 +220,8 @@ bool Gap()
     delay(2000);
   } else {return false;}
 
+  // Check if over the line and turn RIGHT.
+  //
   if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4] != 1000 && sensors[5] != 1000 && sensors[6] != 1000 && sensors[7] != 1000)
   {
     servor.write(93);
@@ -172,13 +231,15 @@ bool Gap()
     error = position - 1000;
   } else {return true;}
 
+  // Check if over the line, while turning ~180 degrees RIGHT gradually.
+  //
   if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4] != 1000 && sensors[5] != 1000 && sensors[6] != 1000 && sensors[7] != 1000)
   {
         for (unsigned long i=0; i < 1000; i++)
         {
           position = qtr.readLine(sensors);
           error = position - 1000;
-          //SensorPrint();
+          
           if (sensors[1] != 1000 && sensors[2] != 1000 && sensors[3] != 1000 && sensors[4] != 1000 && sensors[5] != 1000 && sensors[6] != 1000 && sensors[7] != 1000)
             {
               servor.write(90);
@@ -188,6 +249,8 @@ bool Gap()
         } 
   } else {return true;}
 
+  // Check if over the line and turn LEFT. The robot should have already have found the line, so probably won't get to here.
+  //
   if (sensors[1] == 0 && sensors[2] == 0 && sensors[3] == 0 && sensors[4] == 0 && sensors[5] == 0 && sensors[6] == 0 && sensors[7] == 0)
   {
     servor.write(93);
@@ -209,6 +272,9 @@ bool Gap()
     } else {return true;}
 } 
 
+
+// FOUND YELLOW LINE
+//
 void Colour(int turn, int drive)
 {
   ColourPrint();
@@ -278,10 +344,6 @@ void Colour(int turn, int drive)
       Drive();
     }
    }
-   
-    
-
-  
 }
 
 
