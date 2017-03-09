ea5a6d09dfd51433ccbd1f9beebf5caf6d7a3df4
Jonathan Perkins <jonathanperkins30@gmail.com>
2017-03-09 14:37:56 +0000

    Update Nano.ino

--- MAIN/Nano.ino
+++ MAIN/Nano.ino
@@ -8,7 +8,17 @@
 #include "Adafruit_TCS34725.h"
 
 /* Initialise with specific int time and gain values */
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+
+int rMin  = 255;
+int rMax = 0;
+int gMin  = 255;
+int gMax = 0;
+int bMin  = 255;
+int bMax = 0;
+
+int cal = false;
 
 void setup(void) {
   Serial.begin(9600);
@@ -16,6 +26,7 @@ void setup(void) {
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
   
   if (tcs.begin()) {
     Serial.println("Found sensor");
@@ -26,41 +37,96 @@ void setup(void) {
 }
 
 void loop(void) {
-  uint16_t r, g, b, c, colorTemp, lux;
+
+  if (cal == false)
+  {
+    for (unsigned long i = 0; i < 300; i++)
+    {
+      cali();
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    cal = true;
+  }
   
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature(r, g, b);
-  lux = tcs.calculateLux(r, g, b);
+  uint16_t red, green, blue, c, colorTemp, lux;
+  tcs.getRawData(&red, &green, &blue, &c);
+  colorTemp = tcs.calculateColorTemperature(red, green, blue);
+  lux = tcs.calculateLux(red, green, blue);
+
+  red= map(red,rMin,rMax,0,255);
+  red = constrain(red,0,255);
+
+  green= map(green,gMin,gMax,0,255);
+  green = constrain(green,0,255);
+
+  blue = map(blue,bMin,bMax,0,255);
+  blue = constrain(blue,0,255);
 
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
   digitalWrite(12, LOW);
- 
-  Serial.println(c);
+
+  Serial.print("red = ");
+  Serial.println(red);
+  Serial.print("blue = ");
+  Serial.println(blue);
+  Serial.print("green = ");
+  Serial.println(green);
   
-  if (c >= 5500 && c < 8500) // BLUE
+  if (red < 70 && blue < 100 && blue > 10 && green < 70 && green > 10 ) // BLUE
   {
     Serial.println("BLUE");
     digitalWrite(9, HIGH);
-    delay(10);
   }
-   if (c >= 10000 && c < 13000) //RED
+   if (red > 70 && blue < 70 && green < 70) //RED
   {
     Serial.println("RED");
     digitalWrite(10, HIGH);
     delay(10);
   }
-  if (c < 5000) // BLACK
-  {
-    Serial.println("BLACK");
-    digitalWrite(11, HIGH);
-    delay(10);
-  }
-  if (c >= 20000 && c < 40000) // YELLOW
+//  if (red < 20 && blue < 20) // BLACK
+//  {
+//    Serial.println("BLACK");
+//    digitalWrite(11, HIGH);
+//  }
+  if (red <= 255 && red > 180 && green <= 255 && green > 140 && blue > 70 && blue < 120) // YELLOW
   {
     Serial.println("YELLOW");
     digitalWrite(12, HIGH);
     delay(10);
   }
+}
+
+
+void cali() {
+  uint16_t red, green, blue, c, colorTemp, lux;
+  tcs.getRawData(&red, &green, &blue, &c);
+  
+      // record the maximum sensor value
+      if (red > rMax) {
+        rMax = red;
+      }
+      // record the minimum sensor value
+      if (red < rMin) {
+        rMin = red;
+      }
+      
+      if (green > gMax) {
+        gMax = green;
+      }
+      // record the minimum sensor value
+      if (green < gMin) {
+        gMin = green;
+      }
+
+      if (blue > bMax) {
+        bMax = blue;
+      }
+      // record the minimum sensor value
+      if (blue < bMin) {
+        bMin = blue;
+      }
 }
